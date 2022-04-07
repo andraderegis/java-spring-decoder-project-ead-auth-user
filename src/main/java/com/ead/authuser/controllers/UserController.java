5e5db9d60eebcb7239c048ffcb0af40d41288c68
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-04-06 22:32:59 -0300

    fix: fix delete route mapping

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +40,7 @@ public class UserController {
 
   }
 
-  @GetMapping("/{id}")
+  @DeleteMapping("/{id}")
   public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") UUID id) {
     Optional<UserModel> userModelOptional = userService.findById(id);
 
