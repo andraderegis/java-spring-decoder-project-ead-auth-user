3ac5899bb2b5d92c860dc78edce2040e3b998447
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-04-06 21:22:43 -0300

    feat: added findAll, findById and delete service methods

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package com.ead.authuser.services.impl;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 
@@ -11,4 +16,19 @@ public class UserServiceImpl implements UserService {
 
   @Autowired
   private UserRepository userRepository;
+
+  @Override
+  public List<UserModel> findAll() {
+    return this.userRepository.findAll();
+  }
+
+  @Override
+  public Optional<UserModel> findById(UUID userId) {
+    return this.userRepository.findById(userId);
+  }
+
+  @Override
+  public void delete(UUID id) {
+    this.userRepository.deleteById(id);
+  }
 }
