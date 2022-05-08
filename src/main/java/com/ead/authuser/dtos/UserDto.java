59c980ff2637f7836df08996170ee1d92d5e6c5b
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-05-08 19:38:40 -0300

    feat: added email custom validation

--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.ead.authuser.validation.UsernameConstraint;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -35,12 +36,13 @@ public class UserDto {
   // Essas visões foram definidas por meio das interfaces UserView e suas filhas
   @NotBlank(groups = UserView.RegistrationPost.class)
   @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
+  @UsernameConstraint(groups = UserView.RegistrationPost.class)
   @JsonView(UserView.RegistrationPost.class)
   private String username;
 
   @NotBlank(groups = UserView.RegistrationPost.class)
   @Size(max = 50, groups = UserView.RegistrationPost.class)
-  @Email
+  @Email(groups = UserView.RegistrationPost.class)
   @JsonView(UserView.RegistrationPost.class)
   private String email;
 
