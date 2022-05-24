e0f735e89b53b982ae210f18497d0a47b38da6ff
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-05-23 21:46:36 -0300

    feat: added api pagination on list users

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -9,6 +9,8 @@ import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -34,16 +36,21 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public void save(UserModel userModel) {
-    userRepository.save(userModel);
+    this.userRepository.save(userModel);
   }
 
   @Override
   public boolean existsByUsername(String username) {
-    return userRepository.existsByUsername(username);
+    return this.userRepository.existsByUsername(username);
   }
 
   @Override
   public boolean existsByEmail(String email) {
-    return userRepository.existsByEmail(email);
+    return this.userRepository.existsByEmail(email);
+  }
+
+  @Override
+  public Page<UserModel> findAll(Pageable pageable) {
+    return this.userRepository.findAll(pageable);
   }
 }
