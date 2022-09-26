1444cd05fed6cac7269b72e90d4eabf350c6df8b
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-09-25 21:09:42 -0300

    feat: add UserCoureRepository and UserCourseServices

---
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ead.authuser.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.services.UserCourseService;
+
+@Service
+public class UserCourseServiceImpl implements UserCourseService {
+
+  private final UserCourseRepository userCourseRepository;
+
+  public UserCourseServiceImpl(UserCourseRepository userCourseRepository) {
+    this.userCourseRepository = userCourseRepository;
+  }
+}
